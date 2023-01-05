e6e8f031831e64b69e0e2314176c4f2a2a327a0c
Harald Lösing <harald.loesing@bit.admin.ch>
2023-01-05 14:03:48 +0100

    Some code formatting.

--- src/main/java/ch/admin/bag/covidcertificate/web/security/WebSecurityConfig.java
+++ src/main/java/ch/admin/bag/covidcertificate/web/security/WebSecurityConfig.java
@@ -29,7 +29,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requestMatchers().
-                antMatchers("/actuator/**", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").
+                antMatchers(
+                        "/actuator/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**").
                 and().
                 authorizeRequests().anyRequest().permitAll();
     }
